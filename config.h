292a8ce5ce3fac1f5b50de4e77351123d45319b0
KanaMeisa <stewartjobs@outlook.com>
2023-06-22 21:00:05 +0800

    Update Notes

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#006699";
+static const char col_cyan[]        = "#006699";   //#005577
 static const char col_cyan2[]        = "#1E90FF";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
