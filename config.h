e5d972455810014562c04593a4d19c0849dd9266
KanaMeisa <stewartjobs@outlook.com>
2023-07-20 17:29:11 +0800

    Modify font configuration

--- config.h
+++ config.h
@@ -11,16 +11,16 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"Noto Sans CJK JP:size=10", "monospace:size=10"};
-//static const char *fonts[]          = { "Noto Sans CJK JP:size=10", "monospace:size=10" };
+//static const char *fonts[]          = {"monospace:size=10", "Noto Sans CJK JP:size=10"};
 //static const char dmenufont[]       = "monospace:size=10";
 static const char dmenufont[]       = "Noto Sans CJK JP:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]       = "#005577";
-static const char col_cyan2[]        = "#61afef";   
-static const char col_cyan3[]        = "#1E90FF";
+static const char col_cyan[]        = "#005577";
+static const char col_cyan2[]       = "#61afef";   
+static const char col_cyan3[]       = "#1E90FF";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
