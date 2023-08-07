fc54384234f66f9c89f16e842c47b1aa126cf34b
KanaMeisa <stewartjobs@outlook.com>
2023-08-07 16:53:48 +0800

    Change color

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static const char col_cyan3[]       = "#1E90FF";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray1, col_gray2  },
 };
 
 /* tagging */
