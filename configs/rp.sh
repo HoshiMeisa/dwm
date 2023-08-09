28daf23716c0189f9a55a3159622effd897efb77
KanaMeisa <stewartjobs@outlook.com>
2023-08-09 10:14:44 +0800

    Add backup script

---
+++ configs/rp.sh
@@ -0,0 +1,8 @@
+rm ~/dwm/configs/xinitrc
+rm ~/dwm/configs/alacritty.yml
+rm ~/dwm/configs/starship.toml
+
+cp ~/.xinitrc ~/dwm/configs/xinitrc
+cp ~/.config/alacritty/alacritty.yml ~/dwm/configs/alacritty.yml
+cp ~/.config/starship.toml ~/dwm/configs/starship.toml
+
