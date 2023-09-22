86e74ef91866bc31d37861de6cc091089aaf1a01
Meisa Hoshikawa <stewartjobs@outlook.com>
2023-09-22 08:26:36 +0800

    Update settings

--- config.h
+++ config.h
@@ -10,9 +10,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-//static const char *fonts[]          = {"Noto Sans CJK JP:size=10"};
-static const char *fonts[]          = {"monospace:size=10", "Noto Sans CJK JP:size=10"};
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = {"JetBrainsMono:size=10", "Noto Sans CJK JP:size=10"};
+static const char dmenufont[]       = "JetBrainsMono:size=10";
+//static const char *fonts[]          = {"monospace:size=10", "Noto Sans CJK JP:size=10"};
+//static const char dmenufont[]       = "monospace:size=10";
 //static const char dmenufont[]       = "Noto Sans CJK JP:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#333333";
